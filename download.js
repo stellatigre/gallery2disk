dffd1e08a17765c309e9c99c8d626e22fe10be3f
Stella Tigre <stella.cannefax@gmail.com>
2014-03-06 13:22:53 -0600

    I find Javascrpit very convenient for finding elements programmaticaly on webpages - why not do it on the server ?

---
+++ download.js
@@ -0,0 +1,64 @@
+var async   = require('async'),			
+    fs      = require('fs'),
+    cheerio = require('cheerio'),
+    req     = require('request'),
+    galleries = require('./galleries.json'),//run scraper.js first to make this file
+    conf    = require('./config.json');
+
+var cookieJar = req.jar();		// Example used a valid wordpress cookie for this stuff
+var cookie = req.cookie(conf.cookie);
+cookieJar.setCookie(cookie, conf.domain);
+
+// Regex to find the large pictures linked to by the gallery, NOT the thumbnails
+var picRegex = /gallery\/p[0-9]{6,8}\.jpg$/
+
+for (var i=0 ; i < galleries.length ; i++) {
+
+    var gallery = galleries[i];    
+   
+	// this function take a URL for a downloadable file  
+    var download = function(fileUrl) {
+
+		// This is some path / filename mangling specific to my example site.  
+		// It makes correct folders (by gallery) and filenames.
+        var folder = fileUrl.substring(fileUrl.indexOf('gallery/')+7);
+        folder = '.'+folder.slice(0, folder.lastIndexOf('/'))+'/';
+        
+		var fileName = fileUrl.substring(fileUrl.lastIndexOf('/')+1);
+		
+		console.log(folder,fileName); // fun to watch stream everything by and useful during dev
+        
+		// This is the part that handles the actual downloading.
+        req.get({'url' : fileUrl, 'encoding' :'binary'}, function (err, res, body) { 
+            if (!fs.existsSync(folder)) { fs.mkdirSync(folder); } // checks for existing folders/files
+            if (!fs.existsSync(folder+fileName)) {   
+                fs.writeFile(folder+fileName, body, 'binary', function(err) {
+                       if(err) console.log(err);
+                       else console.log(folder+fileName+" saved.");
+                });
+            }
+        });
+    }
+
+    var pictures = [];
+    
+    req.get({
+        'url' : conf.domain+gallery.replace(/\/$/, '-2/'), // again, site-specific path mangling
+        'jar' : cookieJar 								  // use our cookie
+        },
+        function(err, res, body) {
+            if (err) throw err;
+            var $ = cheerio.load(body);		// cheerio parses our response body
+            var aTags = $('a');				// get all <a>'s
+            
+            for (var i=0 ; i < aTags.length; i++) {
+                var href = aTags[i].attribs.href;
+                if (picRegex.test(href)) { pictures.push(href); } // test each link for regex match
+            }
+            
+            console.log(pictures); 
+            async.each(pictures, download, function(err) { console.log(err) ; }); // download() every picture link     
+        }
+    );
+}
+                
---
+++ find_galleries.js
@@ -0,0 +1,28 @@
+// This script finds galleries in a website based on a regular expression in the URLs.  
+// Run it first - works for any website with regular URLs that's organized by gallery ( a lot of them). 
+// Obviously, the regex has to be changed for each site.
+var async   = require('async'),
+    fs      = require('fs'),
+    cheerio = require('cheerio'),
+    req     = require('request');
+	conf	= require('./config.json');
+
+// change this to suit your site
+var galleryRgx = /^\/[a-zA-Z0-9 \-]{2,20}\/?$/	
+var galleries = [];    
+
+req.get(conf.startUrl, function(err, res, body) {
+    if (err) throw err;
+    var $ = cheerio.load(body);
+    var aTags = $('a');			// cheerio uses jQuery selector syntax
+
+    for (var i=0 ; i < aTags.length; i++) {
+        var href = aTags[i].attribs.href;
+        if (galleryRgx.test(href)) { galleries.push(href); }
+    }
+   
+    console.log(galleries, galleries.length+' galleries found.\n');	 // store relative URLs of galleries,
+    fs.writeFileSync('galleries.json', JSON.stringify(galleries)); // to be consumed by download.js
+});
+
+
